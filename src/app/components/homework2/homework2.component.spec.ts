243a5c6f197a35e9c92d69a0f68e1cd2e6a03387
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-04-16 11:47:28 -0600

    homework 2

---
+++ src/app/components/homework2/homework2.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Homework2Component } from './homework2.component';
+
+describe('Homework2Component', () => {
+  let component: Homework2Component;
+  let fixture: ComponentFixture<Homework2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Homework2Component]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(Homework2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/homework2/homework2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homework2',
+  templateUrl: './homework2.component.html',
+  styleUrl: './homework2.component.scss'
+})
+export class Homework2Component {
+  letters = ["A", "B", "C"];
+}
