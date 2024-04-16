2ef2724cc3b86330c01739c69625ffd4f94dca27
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-04-15 18:26:31 -0600

    Homework 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { Hm1Component } from './components/hm1/hm1.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, Hm1Component],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
