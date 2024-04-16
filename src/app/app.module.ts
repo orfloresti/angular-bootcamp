aa2d485086332b45219d6ce79a8c86c00d970918
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-04-16 08:30:33 -0600

    Rename component of homework1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { Hm1Component } from './components/hm1/hm1.component';
+import { Homework1Component } from './components/homework1/homework1.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, Hm1Component],
+  declarations: [AppComponent, Homework1Component],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
