03b20337097a6fd387239eabbdfc0f8e08b725e5
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-04-18 23:24:43 -0600

    Add h2 title and types

--- src/app/components/homework1/homework1.component.ts
+++ src/app/components/homework1/homework1.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class Homework1Component {
 
-  active = false;
+  active: boolean = false;
 
   handleClick() {
     this.active = !this.active
