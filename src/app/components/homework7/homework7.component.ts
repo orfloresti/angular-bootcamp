bac37052876731238d1b8b1eab7a21854ad4a5a4
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-04-23 00:31:53 -0600

    Homework 8

---
+++ src/app/components/homework7/homework7.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homework7',
+  templateUrl: './homework7.component.html',
+  styleUrl: './homework7.component.scss'
+})
+export class Homework7Component {
+  money: number = 34.22;
+  greeting: string = "Hello world!";
+}
