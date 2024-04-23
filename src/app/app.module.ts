3a9a9029f030ed5aa20adcb5e79e7cb6fc392ac7
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-04-23 01:29:55 -0600

    Homework 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { Homework8Component } from './components/homework8/homework8.component';
+import { DialogNameInputComponent } from './components/dialog-name-input/dialog-name-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
+  declarations: [
+    AppComponent,
+    Homework8Component,
+    DialogNameInputComponent
+  ],
   bootstrap: [AppComponent],
+  providers: [
+    provideAnimationsAsync()
+  ],
 })
-export class AppModule {}
+export class AppModule { }
