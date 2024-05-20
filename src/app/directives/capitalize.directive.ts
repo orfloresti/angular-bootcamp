ca51d27b0428de8aca0eef5d50790c00f1cba12d
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-05-20 17:19:22 -0600

    Transform data

--- src/app/directives/capitalize.directive.ts
+++ src/app/directives/capitalize.directive.ts
@@ -1,10 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { capitalizeUtil } from './capitalize.util';
 
 @Directive({
   selector: '[capitalize]',
 })
-export class CapitalizeDirective {
-  constructor(private element:ElementRef) {
-    this.element.nativeElement.style.textTransform = "capitalize";
+export class CapitalizeDirective implements OnInit {
+  @Input() capitalize?: string;
+
+  constructor(private element:ElementRef) {}
+
+  ngOnInit() {
+    const capitalizedText = capitalizeUtil(this.capitalize);
+    this.element.nativeElement.innerText = capitalizedText;
   }
 }
